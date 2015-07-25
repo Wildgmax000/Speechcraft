642e45a1d6b221b4664ee8798345f4387f5ce947
Wildgmax000 <wildgmax000@gmail.com>
2015-07-24 20:58:18 -0400

    Adding Some Items
    I am thinking of including a method in data to help manage the addition
    and removal of items.

--- Speechcraft/src/speechcraft/Data.java
+++ Speechcraft/src/speechcraft/Data.java
@@ -1,12 +1,16 @@
 package speechcraft;
-
-import java.util.List;
 /*
 Format:
-String[0]=name
-String[1]=type
+String[0][0]=name
+String[0][1]=type
 To Be Continued...
 */
 public class Data {
-    static List<String[]> entities;
+    public static String[][] entities;
+    public Data() {
+        entities[0][0]="Sword";
+        entities[0][1]="Item";
+        entities[1][0]="Dragon";
+        entities[1][1]="Animal";
+    }
 }
